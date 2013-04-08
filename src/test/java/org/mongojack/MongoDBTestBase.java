3b6f444f00988fa5f038001898f634674e524662
James Roper <james@jazzy.id.au>
2013-04-08 21:15:29 +1000

    Merged in devbliss update

--- src/test/java/org/mongojack/MongoDBTestBase.java
+++ src/test/java/org/mongojack/MongoDBTestBase.java
@@ -19,22 +19,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.mongojack.JacksonDBCollection;
-
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
 /**
- * Base class for unit tests that run against MongoDB.  Assumes there is a MongoDB instance listening on the default
+ * Base class for unit tests that run against MongoDB. Assumes there is a MongoDB instance listening on the default
  * port on localhost, and that we can do whatever we want to a database called "unittest".
  */
 @RunWith(MongoDBTestCaseRunner.class)
 public abstract class MongoDBTestBase {
     private static final Random rand = new Random();
+    private static final String dbHostKey = "MONGOJACK_TESTDB_HOST";
+    private static final Map<String, String> environment = System.getenv();
+
     private boolean useStreamParser = true;
     private boolean useStreamSerialiser = false;
 
@@ -44,7 +47,12 @@ public abstract class MongoDBTestBase {
 
     @Before
     public void connectToDb() throws Exception {
-        mongo = new Mongo();
+        String testDbHost = "localhost";
+        if (environment.containsKey(dbHostKey)) {
+            testDbHost = environment.get(dbHostKey);
+        }
+
+        mongo = new Mongo(testDbHost);
         db = mongo.getDB("unittest");
         collections = new HashSet<String>();
     }
@@ -69,7 +77,7 @@ public abstract class MongoDBTestBase {
     }
 
     /**
-     * Get a collection with a random name.  Should grant some degree of isolation from tests running in parallel.
+     * Get a collection with a random name. Should grant some degree of isolation from tests running in parallel.
      *
      * @return The collection
      */
@@ -100,7 +108,7 @@ public abstract class MongoDBTestBase {
         }
         return collection;
     }
-    
+
     protected <T, K> JacksonDBCollection<T, K> getCollection(Class<T> type, Class<K> keyType) {
         return configure(JacksonDBCollection.wrap(getCollection(), type, keyType));
     }
