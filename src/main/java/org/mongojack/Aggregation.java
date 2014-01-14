4e95a6e4b7c004852246f29b86094a2730c10cbb
echrist <echrist@localhost.localdomain>
2014-01-14 10:30:32 -0800

    Add license headers, and author information

--- src/main/java/org/mongojack/Aggregation.java
+++ src/main/java/org/mongojack/Aggregation.java
@@ -1,3 +1,16 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mongojack;
 
 import com.mongodb.DBObject;
@@ -7,6 +20,9 @@ import com.mongodb.DBObject;
  * and the return type of the AggregationResult to be specified.
  *
  * @param <T> The type of results to be produced by the aggregation results.
+ * 
+ * @author Christopher Exell
+ * @since 2.1.0
  */
 public class Aggregation<T> {
     private Class<T> resultType;
