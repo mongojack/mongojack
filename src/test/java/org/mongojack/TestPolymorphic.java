d2fe8279465c192d8d28ab038433632dd97aa3a8
Luke Palmer <luke@lukepalmer.net>
2014-04-27 17:03:36 -0400

    Add license

--- src/test/java/org/mongojack/TestPolymorphic.java
+++ src/test/java/org/mongojack/TestPolymorphic.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Luke Palmer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mongojack;
 
 import org.junit.Before;
@@ -12,6 +27,7 @@ import static org.junit.Assert.assertThat;
 
 /**
  * Test use of polymorphic types with mongojack
+ * 
  * @author Luke Palmer
  */
 public class TestPolymorphic extends MongoDBTestBase {
