a2aaa88edb454f84b14d0298fc5aebf9b36a3c7a
dnebing <dnebing@gmail.com>
2018-08-29 15:19:14 -0400

    Add a class for the test cases.

---
+++ src/test/java/org/mongojack/TestJavaTimeHandling.java
@@ -0,0 +1,9 @@
+package org.mongojack;
+
+/**
+ * class TestJavaTimeHandling: Tests the java.time.* handling in MongoJack.
+ *
+ * @author dnebinger 
+ */
+public class TestJavaTimeHandling extends MongoDBTestBase {
+}
